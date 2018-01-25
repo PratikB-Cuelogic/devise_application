03be432fefc5b84c41eb42200369d06f9fa0dfcb
PratikB-Cuelogic <pratik.bamb@Cuelogic.com>
2018-01-25 12:06:31 +0530

    initial commit

---
+++ app/admin/admin_users.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register AdminUser do
+
+  permit_params :email, :password, :password_confirmation
+  config.batch_actions = false
+  config.filters = false
+
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
+end
